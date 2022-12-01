5b5fd6616de23e0002103a3c18b97b2f97ce1d48
陈陆 <969117645@qq.com>
2022-12-01 21:23:03 +0800

    分支测试

---
+++ day02/src/com/stguigu/java/test.java
@@ -0,0 +1,11 @@
+package com.stguigu.java;
+
+/**
+ * @author shkstart
+ * @create 2022-12-01 21:21
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test测试01分支");
+    }
+}
